4289b38ad5af1f45a229a7b723407195f6b001c0
Lornangari <lornangari@gmail.com>
2025-07-27 14:33:19 +0300

    Set up React Router with basic  Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-600 p-4 text-white flex justify-between">
+      <h1 className="font-bold">Neighborhood Connect</h1>
+      <div className="space-x-4">
+        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+        <Link to="/dashboard">Dashboard</Link>
+      </div>
+    </nav>
+  );
+}
+export default Navbar;
